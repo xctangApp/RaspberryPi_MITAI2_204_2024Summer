6631cae81a4baeb3861d3d402ef40f62c01e0fe5
xctangApp <99854995+xctangApp@users.noreply.github.com>
2024-07-18 20:42:56 -0500

    Create TestPythonProgram.py

---
+++ Session04/TestPythonProgram.py
@@ -0,0 +1,13 @@
+#print('What is your name?')  # ask for customer names
+#name = input()
+#print('It is good to meet you, ' + name)
+
+#if name == 'Alice':
+ #   print('Hi, Alice.')
+#else:
+#   print('Hello, stranger.')
+
+spam = 0
+while spam < 5:
+    print('Hello, stanger!')
+    spam = spam + 1
